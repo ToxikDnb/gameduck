73d8acef97ff153688632eae734639d1fbe91453
toxikdnb <toxikdnb@hotmail.com>
2024-11-20 17:34:24 +0000

    Added settings and begun with the colour pallette of the gameboy

---
+++ src/main/java/com/blackaby/Misc/Settings.java
@@ -0,0 +1,12 @@
+package com.blackaby.Misc;
+
+import java.awt.Color;
+
+import com.blackaby.Backend.Emulation.Graphics.GBColor;
+
+public class Settings {
+    public static final GBColor GB_COLOR_0 = new GBColor(224, 248, 208);
+    public static final GBColor GB_COLOR_1 = new GBColor(136, 192, 112);
+    public static final GBColor GB_COLOR_2 = new GBColor(52, 104, 86);
+    public static final GBColor GB_COLOR_3 = new GBColor(8, 24, 32);
+}
