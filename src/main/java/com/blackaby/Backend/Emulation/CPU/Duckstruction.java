88f5eba5154caf5b99f38d9ee36cc1917c276f56
toxikdnb <toxikdnb@hotmail.com>
2025-01-20 22:12:36 +0000

    Added bitwise AND instruction as well as block scope to allow for repeated variable names inside the switch-case

--- src/main/java/com/blackaby/Backend/Emulation/CPU/Duckstruction.java
+++ src/main/java/com/blackaby/Backend/Emulation/CPU/Duckstruction.java
@@ -73,192 +73,248 @@ public class Duckstruction {
         System.out.println("Executing instruction: " + type);
         switch (type) {
             // Loads
-            case REGISTER_REGISTER:
+            case REGISTER_REGISTER: {
                 System.out.println("Setting register " + Register.getRegFrom3Bit(values[1]) + " to value of register "
                         + Register.getRegFrom3Bit(values[0]));
                 cpu.regSet(Register.getRegFrom3Bit(values[1]), cpu.regGet(Register.getRegFrom3Bit(values[0])));
                 break;
-            case IMMEDIATE_REGISTER:
+            }
+            case IMMEDIATE_REGISTER: {
                 cpu.regSet(Register.getRegFrom3Bit(values[0]), values[1]);
                 break;
+            }
             // Loads with memory
-            case MEMORY_REGISTER_HL:
+            case MEMORY_REGISTER_HL: {
                 cpu.regSet(Register.getRegFrom3Bit(values[0]), memory.read(cpu.regGet16(Register.HL)));
                 break;
-            case REGISTER_MEMORY_HL:
+            }
+            case REGISTER_MEMORY_HL: {
                 memory.write(cpu.regGet16(Register.HL), cpu.regGet(Register.getRegFrom3Bit(values[0])));
                 break;
-            case IMMEDIATE_MEMORY_HL:
+            }
+            case IMMEDIATE_MEMORY_HL: {
                 memory.write(cpu.regGet16(Register.HL), values[0]);
                 break;
-            case MEMORY_ACCUMULATOR_BC:
+            }
+            case MEMORY_ACCUMULATOR_BC: {
                 cpu.regSet(Register.A, memory.read(cpu.regGet16(Register.BC)));
                 break;
-            case MEMORY_ACCUMULATOR_DE:
+            }
+            case MEMORY_ACCUMULATOR_DE: {
                 cpu.regSet(Register.A, memory.read(cpu.regGet16(Register.DE)));
                 break;
-            case ACCUMULATOR_MEMORY_BC:
+            }
+            case ACCUMULATOR_MEMORY_BC: {
                 memory.write(cpu.regGet16(Register.BC), cpu.regGet(Register.A));
                 break;
-            case ACCUMULATOR_MEMORY_DE:
+            }
+            case ACCUMULATOR_MEMORY_DE: {
                 memory.write(cpu.regGet16(Register.DE), cpu.regGet(Register.A));
                 break;
-            case MEMORY_ACCUMULATOR_IMMEDIATE:
-                // Values[0] is LSB
+            }
+            case MEMORY_ACCUMULATOR_IMMEDIATE: {
                 cpu.regSet(Register.A, memory.read((values[1] << 8) | values[0]));
                 break;
-            case ACCUMULATOR_MEMORY_IMMEDIATE:
-                // Values[0] is LSB
+            }
+            case ACCUMULATOR_MEMORY_IMMEDIATE: {
                 memory.write((values[1] << 8) | values[0], cpu.regGet(Register.A));
                 break;
-            case MEMORY_ACCUMULATOR_MSB_0xFF_C:
+            }
+            case MEMORY_ACCUMULATOR_MSB_0xFF_C: {
                 cpu.regSet(Register.A, memory.read(0xFF00 + cpu.regGet(Register.C)));
                 break;
-            case ACCUMULATOR_MEMORY_MSB_0xFF_C:
+            }
+            case ACCUMULATOR_MEMORY_MSB_0xFF_C: {
                 memory.write(0xFF00 + cpu.regGet(Register.C), cpu.regGet(Register.A));
                 break;
-            case MEMORY_ACCUMULATOR_MSB_0xFF_IMMEDIATE:
+            }
+            case MEMORY_ACCUMULATOR_MSB_0xFF_IMMEDIATE: {
                 cpu.regSet(Register.A, memory.read(0xFF00 + values[0]));
                 break;
-            case ACCUMULATOR_MEMORY_MSB_0xFF_IMMEDIATE:
+            }
+            case ACCUMULATOR_MEMORY_MSB_0xFF_IMMEDIATE: {
                 memory.write(0xFF00 + values[0], cpu.regGet(Register.A));
                 break;
-            case MEMORY_ACCUMULATOR_HL_DECREMENT:
+            }
+            case MEMORY_ACCUMULATOR_HL_DECREMENT: {
                 cpu.regSet(Register.A, memory.read(cpu.regGet16(Register.HL)));
                 cpu.regSet16(Register.HL, (short) (cpu.regGet16(Register.HL) - 1));
                 break;
-            case ACCUMULATOR_MEMORY_HL_DECREMENT:
+            }
+            case ACCUMULATOR_MEMORY_HL_DECREMENT: {
                 memory.write(cpu.regGet16(Register.HL), cpu.regGet(Register.A));
                 cpu.regSet16(Register.HL, (short) (cpu.regGet16(Register.HL) - 1));
                 break;
-            case MEMORY_ACCUMULATOR_HL_INCREMENT:
+            }
+            case MEMORY_ACCUMULATOR_HL_INCREMENT: {
                 cpu.regSet(Register.A, memory.read(cpu.regGet16(Register.HL)));
                 cpu.regSet16(Register.HL, (short) (cpu.regGet16(Register.HL) + 1));
                 break;
-            case ACCUMULATOR_MEMORY_HL_INCREMENT:
+            }
+            case ACCUMULATOR_MEMORY_HL_INCREMENT: {
                 memory.write(cpu.regGet16(Register.HL), cpu.regGet(Register.A));
                 cpu.regSet16(Register.HL, (short) (cpu.regGet16(Register.HL) + 1));
                 break;
+            }
             // Misc
-            case IMMEDIATE_PAIR:
+            case IMMEDIATE_PAIR: {
                 cpu.regSet16(Register.getRegFrom2Bit(values[0]), (short) ((values[2] << 8) | values[1]));
                 break;
+            }
             // Stack
-            case SP_MEMORY_IMMEDIATE:
+            case SP_MEMORY_IMMEDIATE: {
                 int nn = (values[1] << 8) | values[0];
                 short sp = cpu.regGet(Register.SP);
                 memory.write(nn, (byte) (sp >> 8));
                 memory.write(nn + 1, (byte) sp);
                 break;
-            case HL_SP:
+            }
+            case HL_SP: {
                 cpu.regSet16(Register.SP, cpu.regGet16(Register.HL));
                 break;
-            case STACKPUSH_RR:
-                short currentSPValue = cpu.regGet16(Register.SP);
-                short valToWrite = cpu.regGet16(Register.getRegFrom2Bit(values[0]));
-                memory.stackWrite(currentSPValue - 1, (byte) (valToWrite >> 8));
-                memory.stackWrite(currentSPValue - 2, (byte) valToWrite);
-                cpu.regSet16(Register.SP, (byte) (currentSPValue - 2));
+            }
+            case STACKPUSH_RR: {
+                short sp = cpu.regGet16(Register.SP);
+                short value = cpu.regGet16(Register.getRegFrom2Bit(values[0]));
+                memory.stackWrite(sp - 1, (byte) (value >> 8));
+                memory.stackWrite(sp - 2, (byte) value);
+                cpu.regSet16(Register.SP, (byte) (sp - 2));
                 break;
-            case STACKPOP_RR:
+            }
+            case STACKPOP_RR: {
                 byte lsb = memory.stackRead(cpu.regGet16(Register.SP));
                 byte msb = memory.stackRead((byte) (cpu.regGet16(Register.SP) + 1));
                 short value = (short) ((msb << 8) | lsb);
                 cpu.regSet16(Register.getRegFrom2Bit(values[0]), value);
                 cpu.regSet16(Register.SP, (byte) (cpu.regGet16(Register.SP) + 2));
                 break;
-            case SP_PLUS_IMMEDIATE8_HL:
-                short spValue = cpu.regGet16(Register.SP);
+            }
+            case SP_PLUS_IMMEDIATE8_HL: {
+                short sp = cpu.regGet16(Register.SP);
                 short offset = (short) (values[0]);
-                short result = (short) (spValue + offset);
+                short result = (short) (sp + offset);
                 cpu.regSet16(Register.HL, result);
                 cpu.deactivateFlags(Flag.Z, Flag.N);
-                cpu.setFlag(Flag.H, (spValue & 0xF) + (offset & 0xF) > 0xF);
-                cpu.setFlag(Flag.C, (spValue & 0xFF) + (offset & 0xFF) > 0xFF);
+                cpu.setFlag(Flag.H, (sp & 0xF) + (offset & 0xF) > 0xF);
+                cpu.setFlag(Flag.C, (sp & 0xFF) + (offset & 0xFF) > 0xFF);
                 break;
+            }
             // Additions
-            case ADD_REGISTER_ACCUMULATOR:
+            case ADD_REGISTER_ACCUMULATOR: {
                 cpu.alu.Add(Register.getRegFrom3Bit(values[0]), Register.A);
                 break;
-            case ADD_MEMORY_HL:
+            }
+            case ADD_MEMORY_HL: {
                 cpu.alu.Add(memory.read(cpu.regGet16(Register.HL)), Register.A);
                 break;
-            case ADD_IMMEDIATE:
+            }
+            case ADD_IMMEDIATE: {
                 cpu.alu.Add(values[0], Register.A);
                 break;
+            }
             // Additions with carry
-            case ADC_REGISTER:
+            case ADC_REGISTER: {
                 cpu.alu.Add(cpu.getFlag(Flag.C), Register.A);
                 cpu.alu.Add(Register.getRegFrom3Bit(values[0]), Register.A);
                 break;
-            case ADC_MEMORY_HL:
+            }
+            case ADC_MEMORY_HL: {
                 cpu.alu.Add(cpu.getFlag(Flag.C), Register.A);
                 cpu.alu.Add(memory.read(cpu.regGet16(Register.HL)), Register.A);
                 break;
-            case ADC_IMMEDIATE:
+            }
+            case ADC_IMMEDIATE: {
                 cpu.alu.Add(cpu.getFlag(Flag.C), Register.A);
                 cpu.alu.Add(values[0], Register.A);
                 break;
+            }
             // Subtractions
-            case SUB_REGISTER:
+            case SUB_REGISTER: {
                 cpu.alu.Sub(Register.getRegFrom3Bit(values[0]), Register.A);
                 break;
-            case SUB_MEMORY_HL:
+            }
+            case SUB_MEMORY_HL: {
                 cpu.alu.Sub(memory.read(cpu.regGet16(Register.HL)), Register.A);
                 break;
-            case SUB_IMMEDIATE:
+            }
+            case SUB_IMMEDIATE: {
                 cpu.alu.Sub(values[0], Register.A);
                 break;
+            }
             // Subtractions with carry
-            case SBC_REGISTER:
+            case SBC_REGISTER: {
                 cpu.alu.Sub(cpu.getFlag(Flag.C), Register.A);
                 cpu.alu.Sub(Register.getRegFrom3Bit(values[0]), Register.A);
                 break;
-            case SBC_MEMORY_HL:
+            }
+            case SBC_MEMORY_HL: {
                 cpu.alu.Sub(cpu.getFlag(Flag.C), Register.A);
                 cpu.alu.Sub(memory.read(cpu.regGet16(Register.HL)), Register.A);
                 break;
-            case SBC_IMMEDIATE:
+            }
+            case SBC_IMMEDIATE: {
                 cpu.alu.Sub(cpu.getFlag(Flag.C), Register.A);
                 cpu.alu.Sub(values[0], Register.A);
                 break;
-            // Other arithmetic functions
-            case CP_REGISTER:
-                byte cpFromReg = cpu.regGet(Register.A);
-                byte cpWithReg = cpu.regGet(Register.getRegFrom3Bit(values[0]));
-                short cpResultReg = (short) (cpFromReg - cpWithReg);
-                cpu.alu.updateSubFlags(cpResultReg, cpFromReg, cpWithReg);
+            }
+            // Comparison functions
+            case CP_REGISTER: {
+                byte from = cpu.regGet(Register.A);
+                byte with = cpu.regGet(Register.getRegFrom3Bit(values[0]));
+                short result = (short) (from - with);
+                cpu.alu.updateSubFlags(result, from, with);
                 break;
-            case CP_MEMORY_HL:
-                byte cpFromMem = cpu.regGet(Register.A);
-                byte cpWithMem = memory.read(cpu.regGet16(Register.HL));
-                short cpResultMem = (short) (cpFromMem - cpWithMem);
-                cpu.alu.updateSubFlags(cpResultMem, cpFromMem, cpWithMem);
+            }
+            case CP_MEMORY_HL: {
+                byte from = cpu.regGet(Register.A);
+                byte with = memory.read(cpu.regGet16(Register.HL));
+                short result = (short) (from - with);
+                cpu.alu.updateSubFlags(result, from, with);
                 break;
-            case CP_IMMEDIATE:
-                byte cpFromImmediate = cpu.regGet(Register.A);
-                byte cpWithImmediate = values[0];
-                short cpResultImmediate = (short) (cpFromImmediate - cpWithImmediate);
-                cpu.alu.updateSubFlags(cpResultImmediate, cpFromImmediate, cpWithImmediate);
+            }
+            case CP_IMMEDIATE: {
+                byte from = cpu.regGet(Register.A);
+                byte with = values[0];
+                short result = (short) (from - with);
+                cpu.alu.updateSubFlags(result, from, with);
                 break;
-            case INC_REGISTER:
-                Register incReg = Register.getRegFrom3Bit(values[0]);
-                cpu.regSet(incReg, cpu.idu.calculate(cpu.regGet(incReg), true));
+            }
+            // IDU functions
+            case INC_REGISTER: {
+                Register reg = Register.getRegFrom3Bit(values[0]);
+                cpu.regSet(reg, cpu.idu.calculate(cpu.regGet(reg), true));
                 break;
-            case INC_MEMORY_HL:
-                short incAddr = cpu.regGet16(Register.HL);
-                memory.write(incAddr, cpu.idu.calculate(memory.read(incAddr), true));
+            }
+            case INC_MEMORY_HL: {
+                short address = cpu.regGet16(Register.HL);
+                memory.write(address, cpu.idu.calculate(memory.read(address), true));
                 break;
-            case DEC_REGISTER:
-                Register decReg = Register.getRegFrom3Bit(values[0]);
-                cpu.regSet(decReg, cpu.idu.calculate(cpu.regGet(decReg), true));
+            }
+            case DEC_REGISTER: {
+                Register reg = Register.getRegFrom3Bit(values[0]);
+                cpu.regSet(reg, cpu.idu.calculate(cpu.regGet(reg), true));
                 break;
-            case DEC_MEMORY_HL:
-                short decAddr = cpu.regGet16(Register.HL);
-                memory.write(decAddr, cpu.idu.calculate(memory.read(decAddr), true));
+            }
+            case DEC_MEMORY_HL: {
+                short address = cpu.regGet16(Register.HL);
+                memory.write(address, cpu.idu.calculate(memory.read(address), true));
                 break;
-            default:
+            }
+            // Bitwise functions
+            case AND_REGISTER: {
+                byte result = (byte) ((byte) cpu.regGet(Register.A)
+                        & (byte) cpu.regGet(Register.getRegFrom3Bit(values[0])));
+                cpu.regSet(Register.A, result);
+                if (result == 0) {
+                    cpu.setFlag(Flag.Z, true);
+                }
+                cpu.setFlag(Flag.H, true);
+                cpu.deactivateFlags(Flag.N, Flag.C);
+                break;
+            }
+            default: {
                 System.err.println("Unsupported load type: " + type);
+            }
         }
 
     }
