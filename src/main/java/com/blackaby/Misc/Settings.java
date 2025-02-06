c6f467e53d680831c64b5e475b7659b214cbee1d
toxikdnb <toxikdnb@hotmail.com>
2025-02-06 16:31:29 +0000

    Added GBColors as objects to increae efficiency

--- src/main/java/com/blackaby/Misc/Settings.java
+++ src/main/java/com/blackaby/Misc/Settings.java
@@ -1,8 +1,15 @@
 package com.blackaby.Misc;
 
+import com.blackaby.Backend.Emulation.Graphics.GBColor;
+
 public class Settings {
     public static final String GB_COLOR_0 = "#E0F8D0";
     public static final String GB_COLOR_1 = "#88C070";
     public static final String GB_COLOR_2 = "#346856";
     public static final String GB_COLOR_3 = "#081820";
+
+    public static final GBColor GB_COLOR_0_OBJ = new GBColor(GB_COLOR_0);
+    public static final GBColor GB_COLOR_1_OBJ = new GBColor(GB_COLOR_1);
+    public static final GBColor GB_COLOR_2_OBJ = new GBColor(GB_COLOR_2);
+    public static final GBColor GB_COLOR_3_OBJ = new GBColor(GB_COLOR_3);
 }
